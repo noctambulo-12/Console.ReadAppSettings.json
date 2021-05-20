c9b695627ec83476b344aba79aa3e7e49726b1c4
Elmer <elmer@cangahuala.dev>
2021-05-20 01:47:48 -0500

    Implement Console Read AppSettings.json

--- ReadAppSettings.json/Program.cs
+++ ReadAppSettings.json/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ReadAppSettings.json
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public static IConfigurationRoot Configuration;
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            Console.WriteLine(Configuration.GetSection("DbConnectionConfig").GetSection("ServerName").Value);
+            Console.WriteLine(Configuration.GetSection("DbConnectionConfig").GetSection("DatabaseName").Value);
+            Console.WriteLine(Configuration.GetSection("DbConnectionConfig").GetSection("UserName").Value);
+            Console.WriteLine(Configuration.GetSection("DbConnectionConfig").GetSection("Password").Value);
+
+            Console.WriteLine(Configuration.GetConnectionString("DataConnection"));
+
+            Console.ReadLine();
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+            serviceCollection.AddSingleton(Configuration);
         }
     }
 }
